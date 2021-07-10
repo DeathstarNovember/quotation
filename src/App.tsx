b03f6a0d055d78311a92f8db0e25f413e0afb5d1
John <john@trabian.com>
2021-07-10 13:09:40 -0400

    Adds mode change buttons and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const URL_BASE = 'https://zenquotes.io/'
 
-const QUOTES_API_URL = `${URL_BASE}/api/`
+const QUOTES_API_URL = `${URL_BASE}/api`
 
 const OTD_API_URL = `https://apizen.date/`
 
-const modes = ['random', 'today', 'quotes']
-
 const attributionText = 'powered by zenquotes.io'
 
+type Mode = 'random' | 'today' | 'quotes' | 'onThisDay'
+
+type Quote = {
+  /** The quote text */
+  q: string
+  /** The name of the author of the quote. */
+  a: string
+  /** A preformatted HTML element for presenting the quote. */
+  h: string
+}
+
 const App = () => {
+  const quotes: Quote[] = []
+
+  const [mode, setMode] = useState<undefined | Mode>(undefined)
+
+  fetch(QUOTES_API_URL)
+
   return (
     <div className="App">
+      {mode ? <div>{mode}</div> : null}
+      <button
+        onClick={() => {
+          setMode('today')
+        }}
+      >
+        Today
+      </button>
+      <button
+        onClick={() => {
+          setMode('random')
+        }}
+      >
+        Random
+      </button>
+      <button
+        onClick={() => {
+          setMode('quotes')
+        }}
+      >
+        List
+      </button>
+      <button
+        onClick={() => {
+          setMode('onThisDay')
+        }}
+      >
+        On This Day
+      </button>
       <p>
         <a href={URL_BASE}>{attributionText}</a>
       </p>
